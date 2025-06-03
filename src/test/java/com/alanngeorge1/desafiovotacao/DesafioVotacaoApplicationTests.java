347f2d85e788f252ee3a8c03ffbb1caf2c16ca7f
Alan George <alanngeorge1@gmail.com>
2025-06-03 17:08:43 -0300

    Configuração inicial do projeto

---
+++ src/test/java/com/alanngeorge1/desafiovotacao/DesafioVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanngeorge1.desafiovotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
